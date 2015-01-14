48c79a79e8f942bc518c2b9e7e52de34fbe1461f
chrkolst <christopher.kolstad@finn.no>
2015-01-14 14:20:31 +0100

    Make examples runnable

--- rxjava-cookbook/src/main/java/com/github/eirslett/reactivecookbook/rxjava/java/AsyncHttp.java
+++ rxjava-cookbook/src/main/java/com/github/eirslett/reactivecookbook/rxjava/java/AsyncHttp.java
@@ -4,19 +4,17 @@ import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.nio.client.methods.HttpAsyncMethods;
 
-public class AsyncHttpJava {
+public class AsyncHttp {
 
-  public Observable<String> getBody(String url, HttpAsyncClient client) {
-    return ObservableHttp.createRequest(HttpAsyncMethods.createGet(url, httpClient)
-          .toObservable()
-          .flatMap((r) -> {
-            r.getContent().map(String::new)
-          });
+  public static Observable<String> getBody(String url, CloseableHttpAsyncClient client) {
+    return ObservableHttp.createRequest(HttpAsyncMethods.createGet(url), client)
+        .toObservable()
+        .flatMap(r -> r.getContent().map(String::new));
   }
-  
+
   public static void main(String... args) {
-    HttpAsyncClient client = HttpAsyncClients.createDefault();
+    CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
     client.start();
-    getBody("http://www.wikipedia.com", client).subscribe((body) -> System.out.println(body), () -> {}, () -> client.close());
+    getBody("http://www.wikipedia.com", client).subscribe((body) -> System.out.println(body));
   }
 }
